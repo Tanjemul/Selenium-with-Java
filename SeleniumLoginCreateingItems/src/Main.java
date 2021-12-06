229544d6216121d99e13587b1be974dfcf000aa4
Tanjemul Islam <78335782+Tanjemul@users.noreply.github.com>
2021-12-06 23:20:32 +0600

    Updated fro

--- SeleniumLoginCreateingItems/src/Main.java
+++ SeleniumLoginCreateingItems/src/Main.java
@@ -10,15 +10,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	System.setProperty("webdriver.chrome.driver", "E:\\one drive\\OneDrive - Fintech Solutions\\Desktop\\chromedriver_win32\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "E:\\******\\Desktop\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	
 	driver.manage().window().maximize();
-	driver.get("http://59.152.61.37:11097/");
+	driver.get("http://**.***.***.***:****/");
 	System.out.println(driver.getTitle());
 	WebElement id = driver.findElement(By.className(null).cssSelector("#account > div:nth-child(3) > input"));
 	
-	id.sendKeys("admin@idtp");
+	id.sendKeys("admin");
 	WebElement pass =driver.findElement(By.cssSelector("#account > div:nth-child(4) > input"));
 	pass.sendKeys("123456");
 	
